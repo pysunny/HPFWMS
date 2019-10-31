a5ff75e8382673c0ffe0ebc05ab42e2fd637e1eb
SUNNY <huf_sunny>
2019-10-31 16:30:47 +0800

    init webide

--- apps/standard/views.py
+++ apps/standard/views.py
@@ -195,27 +195,11 @@ class PicsView(View):
             return JsonResponse({'res': 0, 'errmsg': '数据不完整'})
 
         # 检验是否有相同名字
-        # 如果是修改模式，需要排除自己
-        if pic_id:
-            try:
-                pic = PicsModels.objects.filter(name=name, wheel=wheel).exclude(id=pic_id)
-            except PicsModels.DoesNotExist:
-                # 用户名不存在
-                pic = None
-            if pic:
-                return JsonResponse({'res': 1, 'errmsg': '此图元名称已经存在'})
-            # 检验是否有结构的屏风图元
-            try:
-                pic = PicsModels.objects.filter(leftside=leftside, middle=middle, wheel=wheel, rightside=rightside).exclude(id=pic_id)
-            except PicsModels.DoesNotExist:
-                # 用户名不存在
-                pic = None
-            if pic:
-                return JsonResponse({'res': 3, 'errmsg': '此图元结构已经存在'})
-        
-        else:
             try:
                 pic = PicsModels.objects.filter(name=name, wheel=wheel)
+                # 如果是修改模式，需要排除自己
+                if pic_id:
+                    pic = pic.exclude(id=pic_id)
             except PicsModels.DoesNotExist:
                 # 用户名不存在
                 pic = None
@@ -224,12 +208,15 @@ class PicsView(View):
             # 检验是否有结构的屏风图元
             try:
                 pic = PicsModels.objects.filter(leftside=leftside, middle=middle, wheel=wheel, rightside=rightside)
+                # 如果是修改模式，需要排除自己
+                if pic_id:
+                    pic = pic.exclude(id=pic_id)
             except PicsModels.DoesNotExist:
                 # 用户名不存在
                 pic = None
             if pic:
                 return JsonResponse({'res': 3, 'errmsg': '此图元结构已经存在'})
-
+        
         # 业务处理
         # 如果pic_id有值，表示是修改模式，需要使用更新
 
@@ -323,9 +310,11 @@ class PicsPartsView(View):
         if not all([name, paneltype, panelpart, svgcode]):
             return JsonResponse({'res': 0, 'errmsg': '数据不完整'})
 
-        # 检验已有相同名字
+        # 检验已有相同名字,# 如果修改模式要排除本身
         try:
-            picpart = PartPicModels.objects.get(name=name, panelpart=panelpart)
+            picpart = PartPicModels.objects.filter(name=name, panelpart=panelpart)
+            if picparts_id:
+                picpart = picpart.exclude(id=picparts_id)
         except PartPicModels.DoesNotExist:
             # 用户名不存在
             picpart = None
@@ -340,7 +329,8 @@ class PicsPartsView(View):
             paneltype=paneltype,
             panelpart=panelpart,
             svgcode=svgcode
-        )
+            )
+            return JsonResponse({'res': 2})
 
         # 业务处理
         picpart = PartPicModels.objects.create(
